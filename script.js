4aec002644ebe054f5ce7c2e6dcfa120b379571c
Priti2510 <113327853+Priti2510@users.noreply.github.com>
2023-12-31 13:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+document.getElementById('myForm').addEventListener('submit', function(event) {
+ event.preventDefault();
+
+ var name = document.getElementById('name').value;
+ var year = document.getElementById('year').value;
+
+ var url = 'https://localhost:8080/?name=' + encodeURIComponent(name) + '&year=' + encodeURIComponent(year);
+
+ document.getElementById('url').textContent = url;
+});
